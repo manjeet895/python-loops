71ac19777ccce0ee34934b5462bb653d3bbccecf
manjeet895 <manjeetsinghrajawat100@gamil.com>
2024-07-26 15:06:41 +0530

    python loops

---
+++ loops.py
@@ -0,0 +1,77 @@
+# count = 1
+# while count <= 5:
+#     print("hello")
+#     count += 1 
+#     # print(count)
+
+    #print number from 1 to 5 
+
+# i =5
+# while i <=6:
+#         print(i)
+#         i -= 1
+#         print("loop ended")
+
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1 
+#qs3
+# n = int(input("enter the number"))
+# i = 1
+# while i <= 10:
+#     print(n*i)
+#     i +=1
+
+#qs4
+# num = [1,4,9,16,25,36,49,64,81,100]
+
+# print(num[0])
+# print(num[1])
+# print(num[2])
+# print(num[3])  
+
+
+# nums = (1,4,9,16,25,36,49,64,81,100)
+# x = 36
+# i = 0
+# while i < len(nums):
+#     if (nums[i]== x):
+#     print("found at idx",i)
+# else:
+#     print("finding ......")
+#     i += 1
+
+# i = 0
+# while i <=5:
+#     if(i == 3):
+#         i+=1
+#         continue
+#     print(i)
+#     i+=1
+
+
+
+# list = [1,2,3,4,5]
+# for val in list:
+#     print(val)
+
+# veggies = ["potato","brinjal","ladyfinger"]
+# for val in veggies:
+#     print(val)
+
+# n= 3
+# fact = 1
+# i = 1
+# while i <= n:
+#     fact *= 1
+#     i += 1
+#     print("factorial = ",fact)
+
+
+n = 5
+fact = 1
+for i in range(1,n+1):
+    fact *= i
+    print("factoral = ",fact)
